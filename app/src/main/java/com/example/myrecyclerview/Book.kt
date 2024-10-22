9fc1ce6e3fae372bec382c6a277216f23c17235f
Salahudin-cloud <ayubi54321@gmail.com>
2024-10-22 20:16:16 +0700

    update ui

--- app/src/main/java/com/example/myrecyclerview/Book.kt
+++ app/src/main/java/com/example/myrecyclerview/Book.kt
@@ -1,11 +1,18 @@
 package com.example.myrecyclerview
 
+
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+
 @Parcelize
 data class Book(
-    val name: String,
-    val description: String,
-    val photo: Int
+    val photo : Int,
+    val name:String,
+    val penerbit:String,
+    val terbit:String,
+    val genre:String,
+    val halaman:String,
+    val desc:String,
+
 ) : Parcelable
